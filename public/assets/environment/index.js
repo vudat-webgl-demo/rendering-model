6ab2bc6355b0e2e19d33de02251bfe1d4386d684
Huynh Le Vu Dat <dat.hlv@suntech.local>
2024-06-02 15:43:47 +0700

    update

---
+++ public/assets/environment/index.js
@@ -0,0 +1,13 @@
+export const environments = [
+  {
+    id: "",
+    name: "None",
+    path: null,
+  },
+  {
+    id: "Forest",
+    name: "Forest",
+    path: "./assets/environment/forest.exr",
+    format: ".exr",
+  },
+];
